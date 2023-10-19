b3ee3be633feaaf334ed25f6f52a6f0d2a86952d
Pietro Magni <39495752+tatop@users.noreply.github.com>
2023-10-19 18:40:31 +0200

    F*ck apos

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,22 +6,23 @@ export default function Page() {
       <Navbar />
       <h1 className="text-4xl font-bold pb-5 pt-10">About</h1>
       <p>
-        I'm a huge fan of new bleeding-edge technologies and languages. I've
-        learned a lot about web technologies and core programming, and I strive
-        to learn more about these things every day and utilize my knowledge to
-        understand further how or why the technology around us works.
+        I&apos;m a huge fan of new bleeding-edge technologies and languages.
+        I&apos;ve learned a lot about web technologies and core programming, and
+        I strive to learn more about these things every day and utilize my
+        knowledge to understand further how or why the technology around us
+        works.
       </p>
       <p className="pt-5 pb-10">
-        I'm currently a student at the University of California, Irvine, and I'm
-        pursuing a B.S. in Computer Science. I'm also a part of the Association
-        for Computing Machinery (ACM) at UCI, and I'm a part of the Web
-        Development team.
+        I&apos;m currently a student at the University of California, Irvine,
+        and I&apos;m pursuing a B.S. in Computer Science. I&apos;m also a part
+        of the Association for Computing Machinery (ACM) at UCI, and I&apos;m a
+        part of the Web Development team.
       </p>
       <h1 className="text-3xl font-bold pb-5">Videogames</h1>
       <p>
-        I deeply enjoy playing videogames, and I've been playing them since I
-        was a kid. Recently I've been playing a lot of Valorant and League of
-        Legends recently.
+        I deeply enjoy playing videogames, and I&apos;ve been playing them since
+        I was a kid. Recently I&apos;ve been playing a lot of Valorant and
+        League of Legends recently.
       </p>
     </main>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
     <main className="flex flex-col md:px-24 md:w-3/5 w-full md:mx-auto px-5 mt-24">
       <Headear />
       <p className="pt-10">
-        Welcome to my small corner of the internet. I&aposm 0x23u, and I&aposm a
-        self-taught software engineer interested in web and serverless
+        Welcome to my small corner of the internet. I&apos;m 0x23u, and I&apos;m
+        a self-taught software engineer interested in web and serverless
         technologies and DevOps practices.
       </p>
       <h1 className="pt-20 pb-5 text-lg text-left font-bold">Projects</h1>
       <p>
-        Below are some of projects I&aposve worked on. You can find most of my
+        Below are some of projects I&apos;ve worked on. You can find most of my
         work on my{" "}
         <a
           href="github.com"
@@ -48,7 +48,7 @@ export default function Home() {
             <p className="font-semibold">Screfy</p>
 
             <p className="hidden text-gray-11 md:block">
-              Stay close while you&aposre apart
+              Stay close while you&apos;re apart
             </p>
 
             <span className="h-px flex-1 bg-gray-400" />
